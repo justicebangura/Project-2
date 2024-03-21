87583a21b27ad296bd8b8a1f0f3484fc25aeba84
Justice Bangura <justicebangura@gmail.com>
2024-03-21 19:10:06 -0400

    update

--- backtesting.py
+++
@@ -1,9 +0,0 @@
-# This file contains seperate functions for backtesting the models and strategies
-
-def machine_model(best_model):
-    
-    trained_model_prediction = best_model.predict(X_train_scaled)
-
-    
-    return
-
