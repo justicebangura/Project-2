b7ccc676e59cc5f81d48facaf630ae29a017a17b
Justice Bangura <justicebangura@gmail.com>
2024-03-21 19:03:33 -0400

    update

--- nn_models.py
+++ nn_models.py
@@ -14,7 +14,7 @@ def lstm_model(X_train,y_train):
     "f1_score": f1_score}
 
 
-def ema_model(X_train,y_train):
+def lr_model(X_train,y_train):
     #lets write logic for model here
     return {
     "loss": loss,
@@ -24,27 +24,7 @@ def ema_model(X_train,y_train):
     "f1_score": f1_score}
 
 
-def model_three(X_train,y_train):
-    #lets write logic for model here
-    return {
-    "loss": loss,
-    "accuracy": accuracy,
-    "precision": precision,
-    "recall": recall,
-    "f1_score": f1_score}
-
-
-def model_four(X_train,y_train):
-    #lets write logic for model here
-    return {
-    "loss": loss,
-    "accuracy": accuracy,
-    "precision": precision,
-    "recall": recall,
-    "f1_score": f1_score}
-
-
-def model_five(X_train,y_train):
+def random_tree(X_train,y_train):
     #lets write logic for model here
     return {
     "loss": loss,
